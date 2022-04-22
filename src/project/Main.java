1f18706b2ff989685ee6ff2d96fe14f71d994799
dmytro_lyshtvan <idel_11@me.com>
2022-04-22 19:05:09 +0200

    added loop for the program execution and exit by 'q' char

--- src/project/Main.java
+++ src/project/Main.java
@@ -2,6 +2,8 @@ package project;
 
 import project.handler.Handler;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         Handler.handler();
--- src/project/handler/Handler.java
+++ src/project/handler/Handler.java
@@ -1,13 +1,24 @@
 package project.handler;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Handler {
-    public static void handler(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Введіть число: ");
-        int a = sc.nextInt();
-        System.out.println("Ми ввели a = " + a);
-        sc.close();
+    public static void handler() {
+
+        Scanner scanner = new Scanner(System.in);
+        String insertedDigit = "";
+        char exit = ' ';
+        do {
+            System.out.print("Введіть число: ");
+            insertedDigit = scanner.nextLine().toString();
+            exit = insertedDigit.charAt(0);
+            if (exit == 'q') {
+                System.out.println("Ви ввели q і вийшли з програми");
+            } else  {
+                int a = Integer.parseInt(String.valueOf(insertedDigit));
+                System.out.println("Ми ввели a = " + a);
+            }
+        } while (exit != 'q');
     }
 }

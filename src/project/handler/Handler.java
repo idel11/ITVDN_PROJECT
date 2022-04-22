806534f4999ea56af5f77cd987cbfee126525460
dmytro_lyshtvan <idel_11@me.com>
2022-04-22 16:02:15 +0200

    removed jump to new line

--- src/project/handler/Handler.java
+++ src/project/handler/Handler.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Handler {
     public static void handler(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введіть число: ");
+        System.out.print("Введіть число: ");
         int a = sc.nextInt();
         System.out.println("Ми ввели a = " + a);
         sc.close();

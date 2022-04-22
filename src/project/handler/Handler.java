a31a4a748e6c9f1c1d1eb5fdc561a50bc92f1077
dmytro_lyshtvan <idel_11@me.com>
2022-04-22 20:43:56 +0200

    added second input value and done small refactor

--- src/project/handler/Handler.java
+++ src/project/handler/Handler.java
@@ -1,23 +1,34 @@
 package project.handler;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Handler {
     public static void handler() {
 
         Scanner scanner = new Scanner(System.in);
-        String insertedDigit = "";
-        char exit = ' ';
+        String value;
+        String risk;
+        char exit;
         do {
-            System.out.print("Введіть число: ");
-            insertedDigit = scanner.nextLine().toString();
-            exit = insertedDigit.charAt(0);
+            System.out.print("Введіть значення: ");
+            value = scanner.nextLine();
+            exit = value.charAt(0);
             if (exit == 'q') {
                 System.out.println("Ви ввели q і вийшли з програми");
             } else  {
-                int a = Integer.parseInt(String.valueOf(insertedDigit));
-                System.out.println("Ми ввели a = " + a);
+                System.out.print("Введіть ризик: ");
+                risk = scanner.nextLine();
+                exit = risk.charAt(0);
+                if (exit == 'q') {
+                    System.out.println("Ви ввели q і вийшли з програми");
+                } else {
+
+                    int a = Integer.parseInt(value);
+                    int b = Integer.parseInt(risk);
+                    System.out.println("Ми ввели значення = " + a);
+                    System.out.println("Ми ввели ризик = " + b);
+                    System.out.println("==============================");
+                }
             }
         } while (exit != 'q');
     }
